800e20e8c25a9c84a92bc6b88a604381a5b7e8d5
daniel5u <98783818+nierurein@users.noreply.github.com>
2022-03-06 19:21:50 +0800

    feat: add delete item action

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import {Container, ListGroup, ListGroupItem, Button} from 'reactstrap';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import {v4 as uuidv4} from 'uuid';
 import {connect} from 'react-redux';
-import { getItems } from '../actions/itemActions';
+import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
 
 class ShoppingList extends React.Component {
@@ -12,23 +11,14 @@ class ShoppingList extends React.Component {
     this.props.getItems();
   }
 
+  onDeleteClick = (id) => {
+    this.props.deleteItem(id);
+  }
+
   render() {
     const {items} = this.props.itemReducer;
     return(
       <Container>
-        <Button
-          color='dark'
-          style={{marginBottom: '2rem'}}
-          onClick={() => {
-            const name = prompt('Enter item');
-            if(name) {
-              this.setState(state => ({
-                items: [...state.items, {id: uuidv4(), name}]
-              }));
-            }
-          }}
-        >Add item</Button>
-
         <ListGroup>
           <TransitionGroup className='shopping-list'>
             {items.map(({id, name}) => (
@@ -38,11 +28,7 @@ class ShoppingList extends React.Component {
                     className='remove-btn'
                     color='danger'
                     size='sm'
-                    onClick={() => {
-                      this.setState((state) => ({
-                        items: state.items.filter((item) => item.id !== id)
-                      }));
-                    }}
+                    onClick={this.onDeleteClick.bind(this, id)}
                   >&times;</Button>
                   {name}
                 </ListGroupItem>
@@ -65,4 +51,4 @@ const mapStateToProps = (state) => ({
   itemReducer: state.itemReducer
 });
 
-export default connect(mapStateToProps, {getItems})(ShoppingList);
+export default connect(mapStateToProps, {getItems, deleteItem})(ShoppingList);
